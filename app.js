be509aff3fb367028adff0d2f81416e734a3173c
Alex Robinson <mail@alexmr.me>
2024-05-22 15:20:18 -0400

    Taking a shot at popout streams

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ function displayStreamers(streamers) {
     
     streamers.forEach(streamer => {
         let buttonsHTML = `
-            ${streamer.twitchUrl ? `<a href="${streamer.twitchUrl}" target="_blank" class="btn btn-primary me-2 mb-2">View Twitch</a>` : ''}
-            ${streamer.twitterUrl ? `<a href="${streamer.twitterUrl}" target="_blank" class="btn btn-twitter me-2 mb-2">View Twitter</a>` : ''}
-            ${streamer.tiktokUrl ? `<a href="${streamer.tiktokUrl}" target="_blank" class="btn btn-tiktok me-2 mb-2">View TikTok</a>` : ''}
-            ${streamer.youtubeUrl ? `<a href="${streamer.youtubeUrl}" target="_blank" class="btn btn-youtube me-2 mb-2">View YouTube</a>` : ''}
-            ${streamer.instagramUrl ? `<a href="${streamer.instagramUrl}" target="_blank" class="btn btn-instagram mb-2">View Instagram</a>` : ''}
+            ${streamer.twitterUrl ? `<a href="${streamer.twitterUrl}" target="_blank" class="btn btn-twitter me-2 mb-2">Twitter</a>` : ''}
+            ${streamer.tiktokUrl ? `<a href="${streamer.tiktokUrl}" target="_blank" class="btn btn-tiktok me-2 mb-2">TikTok</a>` : ''}
+            ${streamer.youtubeUrl ? `<a href="${streamer.youtubeUrl}" target="_blank" class="btn btn-youtube me-2 mb-2">YouTube</a>` : ''}
+            ${streamer.instagramUrl ? `<a href="${streamer.instagramUrl}" target="_blank" class="btn btn-instagram mb-2">Instagram</a>` : ''}
         `;
         
         streamersContainer.innerHTML += `
             <div class="col-md-4 mb-4">
                 <div class="card bg-dark text-white text-center">
                     <div class="card-img-container">
-                        <img src="${streamer.image}" class="card-img-top" alt="${streamer.name}">
+                        <img src="${streamer.image}" class="card-img-top streamer-image" alt="${streamer.name}" data-twitch-url="${streamer.twitchUrl}">
                     </div>
                     <div class="card-body d-flex flex-column justify-content-center align-items-center">
                         <h5 class="card-title">${streamer.name}</h5>
@@ -27,6 +26,31 @@ function displayStreamers(streamers) {
                 </div>
             </div>
         `;
+    });
+
+    const streamerImages = document.querySelectorAll('.streamer-image');
+    streamerImages.forEach(image => {
+        image.addEventListener('click', () => {
+            const twitchUrl = image.getAttribute('data-twitch-url');
+            openTwitchPopup(twitchUrl);
+        });
+    });
+}
+
+function openTwitchPopup(twitchUrl) {
+    const popup = document.createElement('div');
+    popup.classList.add('twitch-popup');
+    popup.innerHTML = `
+        <div class="twitch-popup-content">
+            <iframe src="${twitchUrl}" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+            <button class="close-btn">&times;</button>
+        </div>
+    `;
+    document.body.appendChild(popup);
+
+    const closeBtn = popup.querySelector('.close-btn');
+    closeBtn.addEventListener('click', () => {
+        popup.remove();
     });
 }
 
